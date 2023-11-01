a19ba0aad6fedbb60d61b51c817b2a8611be8962
Edi Supriyadi <eddydeniro@users.noreply.github.com>
2023-11-01 21:41:29 +0700

    Update module_top.php

--- modules/custom_module/module_top.php
+++ modules/custom_module/module_top.php
@@ -6,8 +6,8 @@
   $custom_module_table = "app_{$plugin_name}_custom_module";
   if (!isset($_SESSION[$custom_module_table]) || !$_SESSION[$custom_module_table]) 
   {
-      $template_table_query = "CREATE TABLE IF NOT EXISTS `{$custom_module_table}` (`id` int UNSIGNED NOT NULL AUTO_INCREMENT, `parent_id` int UNSIGNED NOT NULL, `is_active` tinyint UNSIGNED NOT NULL, `is_folder` tinyint UNSIGNED NOT NULL, `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, `code` longtext COLLATE utf8mb4_general_ci NOT NULL, `notes` mediumtext COLLATE utf8mb4_general_ci NOT NULL, `sort_order` int UNSIGNED NOT NULL, PRIMARY KEY (`id`), KEY `idx_parent_id` (`parent_id`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
-      is_table_exist($custom_module_table, $template_table_query);
+      $table_query = "CREATE TABLE IF NOT EXISTS `{$custom_module_table}` (`id` int UNSIGNED NOT NULL AUTO_INCREMENT, `parent_id` int UNSIGNED NOT NULL, `is_active` tinyint UNSIGNED NOT NULL, `is_folder` tinyint UNSIGNED NOT NULL, `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, `code` longtext COLLATE utf8mb4_general_ci NOT NULL, `notes` mediumtext COLLATE utf8mb4_general_ci NOT NULL, `sort_order` int UNSIGNED NOT NULL, PRIMARY KEY (`id`), KEY `idx_parent_id` (`parent_id`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
+      db_query($table_query);
       $_SESSION[$custom_module_table] = true;    
   }  
  
